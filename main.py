f99f819afb30185460061fc0d5cefe26c759ed16
AntoScher <asc8er@gmail.com>
2024-11-21 07:28:23 +0300

    driver

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver import Keys
-#Библиотека, которая позволяет вводить данные на сайт с клавиатуры
 from selenium.webdriver.common.by import By
-#Библиотека с поиском элементов на сайте
 import time
 import random
 
